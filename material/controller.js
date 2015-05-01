74e3bb81f6a2bb939e4cfcd553ae678ccedfc5e9
Mario Alberto Villagrana Hurtado <malberto1927@gmail.com>
2015-05-01 13:27:06 -0500

    Se practico con el complemento de material desing que provee angular

---
+++ material/controller.js
@@ -0,0 +1,11 @@
+var app = angular.module('StarterApp', ['ngMaterial'])
+	.config(function($mdThemingProvider) {
+		$mdThemingProvider.theme('default')
+			.primaryPalette('green');
+	});
+
+app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+	$scope.toggleSidenav = function(menuId) {
+		$mdSidenav(menuId).toggle();
+	};
+}]);
