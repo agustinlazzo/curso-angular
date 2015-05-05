1ad72f1f01d3f02048fa70b60902e474297a8dba
Mario Alberto Villagrana Hurtado <malberto1927@gmail.com>
2015-05-05 18:24:30 -0500

    Se genero otra directiva nueva y se trabajo con jquery dentro de angular

--- directives/controller.js
+++ directives/controller.js
@@ -4,6 +4,26 @@
 * Description
 */
 angular.module('CustomDirective', [])
+.directive("myAutocomplete", function(){
+	function link(scope, element, attrs){
+		$(element).autocomplete({
+			source: scope.$eval(attrs.myAutocomplete),
+			select: function(ev, ui){
+				ev.preventDefault();
+				if(ui.item){
+					scope.optionSelected(ui.item.value);
+				}
+			},
+			focus: function(ev, ui){
+				ev.preventDefault();
+				$(this).val(ui.item.label);
+			}
+		});
+	};
+	return {
+		link: link
+	}
+})
 .directive('backImg', function(){
 	return function($scope, element, attrs) {
 		attrs.$observe('backImg', function(value){
@@ -16,11 +36,21 @@ angular.module('CustomDirective', [])
 	}
 })
 .controller('AppController', ['$scope','$http',function(s,h){
-	h.get("https://api.github.com/users/mar10errob/repos")
+	s.repos = [];
+	h.get("https://api.github.com/users/twitter/repos")
 	.success(function(data){
-		s.repos = data;
+		s.posts = data;
+		for (var i = data.length - 1; i >= 0; i--) {
+			var repo = data[i];
+			s.repos.push(repo.name);
+		};
 	})
 	.error(function(error){
 		console.log(error);
-	})
+	});
+	s.optionSelected = function(data){
+		s.$apply(function(){
+			s.main_repo = data;
+		});
+	}
 }]);
